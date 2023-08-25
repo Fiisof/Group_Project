98cb3e10ab487e5bd25ad00337dfe865d657bd1c
zema23 <azeema.bj23@gmail.com>
2023-08-26 04:08:11 +0800

    Added comments and changed from list to tuple

--- tuple_sum.py
+++ tuple_sum.py
@@ -2,18 +2,29 @@
 #         elements in the tuple. For example, if the input is (1,2,3,4,5), the function should return 15.
 
 def tupleSum(t):
-    return sum(t) 
+    return sum(t)
 
-print('Enter integers separated by a comma ",".')
+print('Enter integers separated by a comma ",":')
 user_input = input()
-tup1 = eval(user_input)
-# Converts strings into tuples(integers) internally
 
-print('Your tuple is:')
-print(tup1)
-# Prints tuple
+# Splits the string into a list; specified separator is a comma
+split_input = user_input.split(',')
 
-# Calculates the sum of the tuple
-tuplesum = tupleSum(tup1)
-print('The sum of your tuple is:')
-print(tuplesum)
+tup_list = []                           # Stores the new list
+for i in split_input:
+    try:
+        intcheck = int(i.strip())       # Accepts integers only
+        tup_list.append(intcheck)       # Adds each valid input into the list 'tup1'
+    except ValueError:                  # Overlooks invalid inputs
+        pass
+
+# Converts list into a tuple
+tuple1 = tuple(tup_list)
+
+# Prints the valid inputs from user as a tuple as well as an error message for cases of overlooked inputs
+print('Your valid tuple is:', tuple1)
+print('If there are any missing values, ensure that you input integers only.')
+
+# Calculates and prints the sum of the tuple
+tuplesum = tupleSum(tuple1)
+print('The sum of your valid tuple is:', tuplesum)
