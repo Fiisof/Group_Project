baf99c2440304a215d626ca86ebfe09f49be51df
zema23 <azeema.bj23@gmail.com>
2023-08-26 03:14:20 +0800

    Revert "Changed it to ignore invalid inputs"
    This reverts commit 0e4fdcd1275c05852fafcef626d85e726263b0c3.

--- tuple_sum.py
+++ tuple_sum.py
@@ -1,24 +1,15 @@
 # Task 1: Tuple Sum: Write a python script called ' ' that takes a tuple of integers as input ans returns the sum of all the
 #         elements in the tuple. For example, if the input is (1,2,3,4,5), the function should return 15.
 
-
 def tupleSum(t):
-    return sum(t)
+    return sum(t) 
 
-print('Enter integers separated by a comma ",":')
+print('Enter integers separated by a comma ",".')
 user_input = input()
-split_input = user_input.split(',')
-
-tup1 = []
-for i in split_input:
-    try:
-        intcheck = int(i.strip())
-        tup1.append(intcheck)
-    except ValueError:
-        pass
+tup1 = eval(user_input)
 # Converts strings into tuples(integers) internally
 
-print('Your valid tuple is:')
+print('Your tuple is:')
 print(tup1)
 # Prints tuple
 
