0e4fdcd1275c05852fafcef626d85e726263b0c3
zema23 <azeema.bj23@gmail.com>
2023-08-26 03:08:45 +0800

    Changed it to ignore invalid inputs

--- tuple_sum.py
+++ tuple_sum.py
@@ -1,15 +1,24 @@
 # Task 1: Tuple Sum: Write a python script called ' ' that takes a tuple of integers as input ans returns the sum of all the
 #         elements in the tuple. For example, if the input is (1,2,3,4,5), the function should return 15.
 
-def tupleSum(t):
-    return sum(t) 
 
-print('Enter integers separated by a comma ",".')
+def tupleSum(t):
+    return sum(t)
+
+print('Enter integers separated by a comma ",":')
 user_input = input()
-tup1 = eval(user_input)
+split_input = user_input.split(',')
+
+tup1 = []
+for i in split_input:
+    try:
+        intcheck = int(i.strip())
+        tup1.append(intcheck)
+    except ValueError:
+        pass
 # Converts strings into tuples(integers) internally
 
-print('Your tuple is:')
+print('Your valid tuple is:')
 print(tup1)
 # Prints tuple
 
